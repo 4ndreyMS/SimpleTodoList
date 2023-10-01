5067de3a94d70172cfedfbec6a22ca9a273142b0
4ndreyMS <melendezzsequeira@gmail.com>
2023-10-01 00:35:02 -0600

    adding correct functionality

--- index.js
+++ index.js
@@ -20,12 +20,27 @@ for (i = 0; i < close.length; i++) {
 }
 
 // Add a "checked" symbol when clicking on a list item
+// var list = document.querySelector("ul");
+// list.addEventListener(
+//   "click",
+//   function (ev) {
+//     if (ev.target.tagName === "LI") {
+//       ev.target.classList.add("checked");
+//       const pElement = ev.target.querySelector("p");
+//       pElement.style.textDecoration = "line-through";
+//     }
+//   },
+//   false
+// );
+
 var list = document.querySelector("ul");
 list.addEventListener(
   "click",
   function (ev) {
     if (ev.target.tagName === "LI") {
-      ev.target.classList.toggle("checked");
+      ev.target.classList.add("checked");
+      const pElement = ev.target.querySelector("p");
+      pElement.style.textDecoration = "line-through";
     }
   },
   false
@@ -35,7 +50,8 @@ list.addEventListener(
 function newElement() {
   var li = document.createElement("li");
   var inputValue = document.getElementById("myInput").value;
-  var t = document.createTextNode(inputValue);
+  var t = document.createElement("p");
+  t.textContent = inputValue;
   li.appendChild(t);
   if (inputValue === "") {
     alert("You must write something!");
@@ -44,15 +60,44 @@ function newElement() {
   }
   document.getElementById("myInput").value = "";
 
+  const staticEl = document.getElementById("staticEl");
+  // Check if the element has the "display: none" property
+  if (!staticEl.classList.contains("hide")) {
+    // The element has "display: none"
+    staticEl.classList.add("hide");
+  }
+
   var span = document.createElement("SPAN");
   var txt = document.createTextNode("\u00D7");
   span.className = "close";
   span.appendChild(txt);
-  li.appendChild(span);
+
+  var divTools = document.createElement("div");
+  divTools.classList.add("align-center");
+
+  //   div.appendChild(span);
+  var doneButton = document.createElement("span");
+  doneButton.textContent = "\u2713";
+  doneButton.className = "done";
+
+  // Add click event listeners for the Done and Cancel buttons
+  doneButton.addEventListener("click", function () {
+    const parent = doneButton.parentElement.parentElement;
+    parent.classList.add("checked");
+    const pElement = parent.querySelector("p");
+    pElement.style.textDecoration = "line-through";
+    doneButton.classList.add("hide");
+  });
+
+  divTools.appendChild(doneButton);
+
+  divTools.appendChild(span);
+  li.appendChild(divTools);
+  li.classList.add("align-center");
 
   for (i = 0; i < close.length; i++) {
     close[i].onclick = function () {
-      var div = this.parentElement;
+      var div = this.parentElement.parentElement;
       div.style.display = "none";
     };
   }
